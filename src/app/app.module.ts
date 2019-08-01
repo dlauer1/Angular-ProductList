8ab260e63bb46af8b9d2b4fa8d2feabe916c77fd
dlauer1 <dayne.lauer@yahoo.com>
2019-07-31 19:38:35 -0700

    added rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
